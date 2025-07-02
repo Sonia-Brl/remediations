c2c99c756ee5b984197c3aac1135d6027b4fcd2e
Sonia-Brl <brunelsonia@gmail.com>
2025-07-02 04:25:45 +0200

    Update main home-link

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
                 <div class="flex z-40 bg-default fixed justify-between items-center border-b border-default w-full">
-                <ULink as="button" to="/modules/ui-ux/"  class="p-4"><UIcon name="i-lucide-home" class="size-5" /></ULink>
+                <ULink as="button" to="/modules/ui-ux/"  class="p-4 font-bold flex items-center justify-around"><UIcon name="i-lucide-home mr-2" class="size-5" /><small> Remediations</small> <span class="ml-2 main-home text-primary">by Sonia</span></ULink>
                 <div class="mr-8">
                     <ColorModeButton></ColorModeButton>
                     <USlideover >
