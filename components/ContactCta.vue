f6bc9f7cb441512c12f9c83e40c0c07267fe2523
Sonia-Brl <brunelsonia@gmail.com>
2025-07-08 20:51:39 +0200

    UPD Discord Link

--- components/ContactCta.vue
+++ components/ContactCta.vue
@@ -3,7 +3,7 @@
          <div class="container  m-auto pb-8 md:pe-8 flex flex-row md:me-15 items-center justify-center md:justify-end">
             <div>
                 <p class="font-marker text-left text-white mt-8 pt-0">Une question ? </p>
-                <ULink as="button" to="https://discord.gg/sqMayJeG22" target="_blank" class="contact-cta  flex items-center justify-center text-white text-xl ">Rejoins moi sur <UIcon name="mdi:discord" class="size-10 ms-4" /></ULink>
+                <ULink as="button" to="https://discord.gg/CccQ5jdskn" target="_blank" class="contact-cta  flex items-center justify-center text-white text-xl ">Rejoins moi sur <UIcon name="mdi:discord" class="size-10 ms-4" /></ULink>
             </div>
          </div>
              <img :src="contactSonia" alt="Visuel de Sonia Brunel entrain de travailler" class="hidden md:block absolute bottom-0 z-0 right-0 w-32 lg:w-25" />
