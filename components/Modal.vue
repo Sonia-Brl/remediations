89db66295ab241cc4c1030e8422eeef4f2eb2a9f
Sonia-Brl <brunelsonia@gmail.com>
2025-07-31 10:39:49 +0200

    ADD IA Prompt modul

---
+++ components/Modal.vue
@@ -0,0 +1,18 @@
+<template>
+  <UModal fullscreen>
+    <UButton :label="`${label}`" color="primary"class="scrib text-xl rounded-none float-right text-right font-marker" size="xl" trailing-icon="i-lucide-arrow-right"/>
+
+    <template #body>
+      <slot />
+    </template>
+  </UModal>
+</template>
+
+<script setup>
+defineProps({
+     label: {
+        type: String,
+        default: 'Ouvrir'
+    },
+})
+</script>
---
+++ components/PromptAlert.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div data-orientation="vertical" class="relative overflow-hidden w-full rounded-0 mt-4 p-4 flex gap-2.5 items-start bg-slate-950 dark:text-slate-50 text-inverted">
+            <UIcon name="i-lucide-terminal" class="size-5" />
+            <div class="min-w-0 flex-1 flex flex-col">
+                <div class="text-sm font-bold">{{title}}</div>
+                <div class="text-sm mt-1"> <slot /> </div>
+            </div> 
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        default: 'Defaultqelkfjn title'
+    },
+})
+</script>
